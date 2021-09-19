94ffd3acf8071c9cc2d7cc24a30931ad4cc4910e
Michael <michaelnji34@gmail.com>
2021-09-19 18:31:55 +0100

    added new icons

--- src/_components/icon.svelte
+++ src/_components/icon.svelte
@@ -663,6 +663,36 @@
       d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
     />
   </svg>
+{:else if name == "check"}
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="h-4 w-4 sm:h-6 sm:w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M5 13l4 4L19 7"
+    />
+  </svg>
+{:else if name == "bar-chart"}
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="h-4 w-4 sm:h-6 sm:w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+    />
+  </svg>
 {:else}
   <!-- else content here -->
 {/if}
